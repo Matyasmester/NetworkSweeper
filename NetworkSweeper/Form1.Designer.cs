6094934db08c67323577ddf86c4f7e8090546d4f
Matyasmester <matyas.zoboki@gmail.com>
2022-12-01 19:42:44 +0100

    Initial commit

---
+++ NetworkSweeper/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace NetworkScanner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AdminLabel = new System.Windows.Forms.Label();
+            this.IPBox = new System.Windows.Forms.ListBox();
+            this.IPFromBox = new System.Windows.Forms.TextBox();
+            this.IPToBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AdminLabel
+            // 
+            this.AdminLabel.AutoSize = true;
+            this.AdminLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.AdminLabel.ForeColor = System.Drawing.Color.DarkGreen;
+            this.AdminLabel.Location = new System.Drawing.Point(798, 25);
+            this.AdminLabel.Name = "AdminLabel";
+            this.AdminLabel.Size = new System.Drawing.Size(61, 20);
+            this.AdminLabel.TabIndex = 0;
+            this.AdminLabel.Text = "ADMIN";
+            this.AdminLabel.Visible = false;
+            // 
+            // IPBox
+            // 
+            this.IPBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.IPBox.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.IPBox.FormattingEnabled = true;
+            this.IPBox.ItemHeight = 31;
+            this.IPBox.Location = new System.Drawing.Point(251, 25);
+            this.IPBox.Name = "IPBox";
+            this.IPBox.Size = new System.Drawing.Size(343, 252);
+            this.IPBox.TabIndex = 1;
+            this.IPBox.SelectedIndexChanged += new System.EventHandler(this.IPBox_SelectedIndexChanged);
+            // 
+            // IPFromBox
+            // 
+            this.IPFromBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.IPFromBox.Location = new System.Drawing.Point(28, 321);
+            this.IPFromBox.Name = "IPFromBox";
+            this.IPFromBox.Size = new System.Drawing.Size(343, 35);
+            this.IPFromBox.TabIndex = 2;
+            // 
+            // IPToBox
+            // 
+            this.IPToBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.IPToBox.Location = new System.Drawing.Point(472, 321);
+            this.IPToBox.Name = "IPToBox";
+            this.IPToBox.Size = new System.Drawing.Size(343, 35);
+            this.IPToBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(169, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(628, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To:";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.StartButton.Location = new System.Drawing.Point(173, 396);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(493, 134);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "Start scan";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.StartButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPToBox);
+            this.Controls.Add(this.IPFromBox);
+            this.Controls.Add(this.IPBox);
+            this.Controls.Add(this.AdminLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "NetworkSweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AdminLabel;
+        private System.Windows.Forms.ListBox IPBox;
+        private System.Windows.Forms.TextBox IPFromBox;
+        private System.Windows.Forms.TextBox IPToBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ NetworkSweeper/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Text;
+using System.Security.Principal;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NetworkScanner
+{
+    public partial class MainForm : Form
+    {
+        private bool IsUserElevated = false;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+
+                IsUserElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+
+            if (IsUserElevated) AdminLabel.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void IPBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string MinIPText = IPFromBox.Text;
+            string MaxIPText = IPToBox.Text;
+
+            if (!AreAllValidTextInputs(MinIPText, MaxIPText))
+            {
+                MessageBox.Show("Please put in a valid IP Address!");
+                return;
+            }
+
+            List<IPAddress> allIPS = GetAllIPSInRange(MinIPText, MaxIPText);
+
+            ScanList(allIPS);
+        }
+
+        private async void ScanList(List<IPAddress> ips)
+        {
+            int nSuccesses = 0;
+
+            var tasks = ips.Select(ip => new Ping().SendPingAsync(ip, 2000));
+
+            var results = await Task.WhenAll(tasks);
+
+            foreach(PingReply reply in results)
+            {
+                if (reply.Status == IPStatus.Success)
+                {
+                    IPBox.Items.Add(reply.Address.ToString());
+                    nSuccesses++;
+                }
+            }
+
+            MessageBox.Show("Scan completed, " + nSuccesses + " successful pings.");
+        }
+
+        private List<IPAddress> GetAllIPSInRange(string minText, string maxText)
+        {
+            List<IPAddress> ips = new List<IPAddress>();
+
+            string IPTrimmed = minText.Remove(minText.LastIndexOf('.'));
+
+            int min = GetIPEndValue(minText);
+            int max = GetIPEndValue(maxText);
+
+            if (min > max)
+            {
+                MessageBox.Show("Cannot start scan, start IP is bigger than end IP.");
+                return null;
+            }
+
+            for (int i = min; i <= max; i++)
+            {
+                IPAddress ip = IPAddress.Parse(IPTrimmed + "." + i.ToString());
+
+                ips.Add(ip);
+            }
+
+            return ips;
+        }
+
+        private bool AreAllValidTextInputs(params string[] inputs)
+        {
+            foreach(string input in inputs)
+            {
+                // The user has not entered anything
+                if (string.IsNullOrWhiteSpace(input) || input.Equals(string.Empty)) return false;
+
+                // The user supplied a seemingly incorrect IP address
+                if (input.Split('.').Length != 4) return false;
+            }
+
+            return true;
+        }
+
+        private int GetIPEndValue(string s)
+        {
+            return Convert.ToInt32(s.Substring(s.LastIndexOf('.') + 1));
+        }
+    }
+}
